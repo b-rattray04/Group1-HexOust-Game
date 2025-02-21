7abcd8422668bef072f5717b1272a157549f0b17
WilliamLin <william.lin@ucdconnect.ie>
2025-02-21 14:50:52 +0000

    Sprint 1

--- src/main/java/comp20050/softwareengineeringproject2/HelloController.java
+++ src/main/java/comp20050/softwareengineeringproject2/HelloController.java
@@ -1,6 +1,9 @@
 package comp20050.softwareengineeringproject2;
 
+import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
@@ -8,7 +11,8 @@ import javafx.scene.paint.Color;
 
 
 public class HelloController {
-
+    @FXML
+    private AnchorPane rootPane;
     private Color selectedColor = Color.RED; // Default color
 
     public Color getColor(MouseEvent mouseEvent) {
@@ -27,9 +31,28 @@ public class HelloController {
         selectedColor = getColor(mouseEvent); // Store the selected color
     }
 
-    //changes the color of the hexagons
+    //changes the color of the circles and makes them be on top of the hex board
     public void changeColor(MouseEvent click) {
         Polygon hexagon = (Polygon) click.getSource();
-        hexagon.setFill(selectedColor); // Apply the selected color
+        int hexagonX = (int) (hexagon.getLayoutX() + hexagon.getTranslateX());
+        int hexagonY = (int) (hexagon.getLayoutY() + hexagon.getTranslateY());
+
+        Circle matchingCircle = findCircleByCoords(hexagonX, hexagonY);
+
+        if (matchingCircle != null) {
+            matchingCircle.setFill(Color.RED);
+            matchingCircle.toFront();
+        }
     }
+    private Circle findCircleByCoords(double x, double y) {
+        for (Node node : rootPane.getChildren()) {
+            if (node instanceof Circle && node.getId() != null) {
+                Circle circle = (Circle) node;
+                if (Math.abs(circle.getLayoutX() - x) < 1 && Math.abs(circle.getLayoutY() - y) < 1) {
+                    return circle;
+                }
+            }
+        }
+        return null;
+        }
 }

9a19bb0e35e9e6044d5f2f78cef9e70914e89925
WilliamLin <william.lin@ucdconnect.ie>
2025-02-25 10:36:40 +0000

    implemented CE sounds

--- src/main/java/comp20050/SwEngProject/HexOustApplication.java
+++ src/main/java/comp20050/SwEngProject/HexOustApplication.java
@@ -4,21 +4,46 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
-public class HexOustApplication extends Application {           //To start the game
+import java.io.File;
+
+public class HexOustApplication extends Application { // To start the game
+    private MediaPlayer mediaPlayer; // Declare MediaPlayer as a class-level variable
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("GraphicalUserInterface.fxml"));
         Scene scene = new Scene(root);
-        stage.setScene(scene);
+
+        stage.setScene(scene);              // Set up the stage
         stage.setTitle("HexOust");
         stage.setResizable(false);
         stage.show();
+        playBackgroundMusic();
+    }
+
+    private void playBackgroundMusic() {
+        String audioFilePath =getClass().getResource("/sounds/background.mp3").toString();
+        Media media = new Media(audioFilePath);
+        mediaPlayer = new MediaPlayer(media);
+
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);      //loop
+        mediaPlayer.setVolume(0.5);
+        mediaPlayer.play();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.dispose();
+        }
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-    int check = 0;
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module comp20050.SwEngProject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
 
     opens comp20050.SwEngProject to javafx.fxml;

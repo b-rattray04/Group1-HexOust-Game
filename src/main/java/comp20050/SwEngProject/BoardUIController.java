71a3f681c189120670a78836878ed98b7737a14c
WilliamLin <william.lin@ucdconnect.ie>
2025-02-26 13:01:25 +0000

    trying to revert changes

--- src/main/java/comp20050/SwEngProject/BoardUIController.java
+++ src/main/java/comp20050/SwEngProject/BoardUIController.java
@@ -1,6 +1,5 @@
 package comp20050.SwEngProject;
 
-import javafx.animation.ScaleTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.Node;
@@ -11,8 +10,6 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.scene.media.AudioClip;
-import javafx.util.Duration;
 
 public class BoardUIController {
     @FXML
@@ -23,48 +20,45 @@ public class BoardUIController {
     private Circle gameCircle;
     @FXML
     private Label gameFlag;
-
     private Color selectedColor = Color.RED; // Default color
-    private int player = 0; // 0 = Red Player, 1 = Blue Player
-    AudioClip clip = new AudioClip(getClass().getResource("/sounds/ncp.mp3").toString());
+    private int player = 0;         // 0 = Red Player, 1 = Blue Player
 
     @FXML
     private void initialize() {
-        String css = this.getClass().getResource("/css/style.css").toExternalForm();        //for hover
+        String css = this.getClass().getResource("/css/style.css").toExternalForm();
         rootPane.getStylesheets().add(css);
-
-        closeButton.setOnMouseEntered(event -> {        //enlarged exit
-            ScaleTransition st = new ScaleTransition(Duration.millis(200), closeButton);
-            st.setToX(1.2);
-            st.setToY(1.2);
-            st.play();
-        });
-
-        closeButton.setOnMouseExited(event -> {         //enlarged exit
-            ScaleTransition st = new ScaleTransition(Duration.millis(200), closeButton);
-            st.setToX(1.0);
-            st.setToY(1.0);
-            st.play();
-        });
     }
+    /*
+    *public Color getColor(MouseEvent mouseEvent) {
+    *    Circle circle = (Circle) mouseEvent.getSource();
+    *    Paint fill = circle.getFill(); // Get the fill color
+    *
+    *    if (fill instanceof Color color) { // Ensure it's a Color object
+    *        return color;
+    *    } else {
+    *        return Color.BLUE; // Default fallback
+    *    }
+    *}
+    *unused so far was trying to mess around with a color being taken in and stored when a mouse click is made on the stone
+    *public void selectColor(MouseEvent mouseEvent) {
+    *    selectedColor = getColor(mouseEvent); // Store the selected color
+    *}
+    */
 
-    public void changePlayer() {
-        if (player == 0) {
-            clip.play();
+    public void changePlayer(){
+        if (player == 0){
             selectedColor = Color.BLUE;
             gameCircle.setFill(selectedColor);
             gameFlag.setText("Blue to play");
             player = 1;
-        } else if (player == 1) {
-            clip.play();
+        } else if (player == 1){
             selectedColor = Color.RED;
             gameCircle.setFill(selectedColor);
             gameFlag.setText("Red to play");
             player = 0;
         }
     }
-
-    // Changes the color of the circles and makes them appear on top of the hex board
+    //changes the color of the circles and makes them be on top of the hex board
     public void changeColor(MouseEvent click) {
         Polygon hexagon = (Polygon) click.getSource();
         int hexagonX = (int) (hexagon.getLayoutX() + hexagon.getTranslateX());
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module comp20050.SwEngProject {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.media;
 
 
     opens comp20050.SwEngProject to javafx.fxml;
